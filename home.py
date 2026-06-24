c33ab5938c20cf4faea3f160ccf204116ab06644
David-milltank <daveyuky@gmail.com>
2026-06-25 02:54:08 +0800

    Update home.py

--- home.py
+++ home.py
@@ -125,10 +125,10 @@ elif page == "About Me":
     
     display_gallery(ABOUT_DIR, "Saved About / Profile Images", cols=3, thumb_width=200, caption_prefix="Saved image")
     st.write(" Hi,I'm dave.")
-    st.write("I am the vice president of robotics in my school, and I have been programming for 2 years. I am decent in Python, and I was able to join many competitions. I enjoy playing sports in my f[...]")
+    st.write("I am the vice president of robotics in my school, and I have been programming for 2 years. I am decent in Python, and I was able to join many competitions. I enjoy playing sports in my free time, mainly basketball and bowling. I am passionate about improving my coding skills and interacting with others to create new things like robots or new code to help others.")
     
     st.header("My Passion")
-    st.write("My passion for coding started when my mother signed me up for a coding class when I was still a kid. It was a class for building LEGO robots and coding them to do different tasks. I got [...]
+    st.write("My passion for coding started when my mother signed me up for a coding class when I was still a kid. It was a class for building LEGO robots and coding them to do different tasks. I got hooked immediately but was unable to continue since my family moved to a new house and I was unable to attend it. When I joined Serangoon Garden Secondary School, I was able to join the robotics club and I was able to continue my coding journey and I learnt so much from it. I was able to join many competitions and though I didn't win them, I gained valuable experiences and was able to get the role of robotics vice president. When I heard that my school was offering O-Level Computing, I took it immediately. Though I was not good at it at first, I was able to learn and with help from my teachers and friends I was able to grow even more.")
     
 
 elif page == "Certifications":
@@ -173,9 +173,9 @@ elif page == "Certifications":
 
 elif page == "School":
     st.title("School life")
-    st.write("I Learnt alot in computing ,and the best part about it is going to competitions going to competitions allowed me to learn many new skills and meet alot of new people it boosted my compet[...]
+    st.write("I learnt a lot in Computing, and the best part about it is going to competitions. Going to competitions allowed me to learn many new skills and meet a lot of new people. It boosted my competitive spirit as well. I enjoyed my time in SGSS as I have made many friends and made countless memories from all the events that my school has held, like camp, workshops and inter-class games, which allowed me to build stronger bonds with my classmates.")
     st.subheader("Cca")
-    st.write("I have also learnt a lot from my robotics CCA and made a lot of friends and met a lot of different people in the competitions I have went for. I have went for at least two competitions f[...]
+    st.write("I have also learnt a lot from my robotics CCA and made a lot of friends and met a lot of different people in the competitions I have went for. I have went for at least two competitions for robotics and I have learnt a lot from it. I was also given a great opportunity to talk to Dr. Janil. He came to our school and I was able to represent my CCA with my friends and impressed him with our robot.")
     st.subheader("Awards")
     st.write("i have also went for competitions to represent my school and placed for cross country.")
 
